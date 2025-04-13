7f7ed7f8439acfc92c4909512a949c714d1f373a
DevProdipraj <devprodipraj@gmail.com>
2025-04-13 14:31:23 +0600

    complete login and registation page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Registation from "./pages/Registation";
 import ResetPassword from "./pages/ResetPassword";
 import Error404 from "./pages/Error404";
 
@@ -12,7 +11,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,97 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div className='max-h-screen bg-gradient-to-br form-blue-200' >
-      <h1>Login</h1>
-    </div>
-  )
-}
+  const [login, setLogin] = useState("login");
+  const navigate = useNavigate();
 
-export default Login
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-green-300 via-blue-500 to-purple-400 flex items-center justify-center">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-md text-white min-w-90">
+        {login === "login" ? (
+          <h1 className="text-2xl font-bold text-white text-center">Login</h1>
+        ) : (
+          <h1 className="text-2xl font-bold text-white text-center">
+            Create Account
+          </h1>
+        )}
+        <form action="" method="get">
+          {login === "signup" && (
+            <input
+              className="border-2 border-white mt-4 w-full rounded-md px-2 py-2"
+              placeholder="Full Name"
+              type="text"
+              name="fullName"
+              id="fullName"
+            />
+            
+          )}
+          <input
+            className="border-2 border-white mt-4 w-full rounded-md px-2 py-2"
+            placeholder="Email"
+            type="email"
+            name="userEmail"
+            id="userEmail"
+          />
+          <br />
+          <input
+            className="border-2 border-white mt-4 w-full rounded-md px-2 py-2"
+            placeholder="Password"
+            type="password"
+            name="password"
+            id="passworrd"
+          />
+          {
+            login === "login" && (
+              <input
+              className="text-blue-300 my-4"
+              onClick={() => navigate("/reset-password")}
+              type="button"
+              value="Forgot Password"
+            />
+            )
+          }
+          <br />
+          {login === "login" ? (
+            <input
+              className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-400 py-2 text-xl font-bold cursor-pointer  rounded-full w-full "
+              type="submit"
+              value="Login"
+            />
+          ) : (
+            <input
+              className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-400 py-2  text-xl font-bold cursor-pointer rounded-full w-full mt-5"
+              type="submit"
+              value="Sign Up"
+            />
+          )}
+        </form>
+        <div className="">
+          {login === "login" ? (
+            <p className="my-4 text-sm">
+              Don't hav an account?{" "}
+              <span
+                onClick={() => setLogin("signup")}
+                className="text-blue-400 cursor-pointer"
+              >
+                Sign Up
+              </span>
+            </p>
+          ) : (
+            <p className="my-4 text-sm">
+              Already have an Account?{" "}
+              <span
+                onClick={() => setLogin("login")}
+                className="text-blue-400 cursor-pointer"
+              >
+                Login Here
+              </span>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
