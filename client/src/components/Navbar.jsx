e1c77295738c880d49aa2439dfd3a879c5fc1741
DevProdipraj <devprodipraj@gmail.com>
2025-04-16 16:00:05 +0600

    complete some backend  work

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <div className="container py-5 flex justify-between items-center">
-      <div className="text-2xl font-bold bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 px-3 py-1 bg-clip-text text-transparent">MERN AUTH</div>
+      <div onClick={() => navigate("/")} className="text-2xl font-bold bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 px-3 py-1 bg-clip-text text-transparent cursor-pointer">MERN AUTH</div>
       <div className=" cursor-pointer rounded-md bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 py-1.5 px-5 hover:bg-gray-200 transition-all duration-300">
         <button onClick={()=> navigate("/login")} className="text-xl text-white cursor-pointer" to="#">Login <FaArrowRight className="inline-block" /></button>
       </div>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,43 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [login, setLogin] = useState("login");
   const navigate = useNavigate();
+
+  const {backendUrl, setIsLoggedin} = useContext(AppContext);
+
+  const [login, setLogin] = useState("login");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // console.log(name, email, password);
+
+  const onSubmitHander = async()=> {
+    try {
+      e.preventDefault()
+      axios.default.withCredentials = true;
+      if(state == "Sign up"){
+        const {data} = await axios.post(backendUrl + "/api/auth/register", {
+          name,
+          email,
+          password,
+        });
+        if(data.success){
+          setIsLoggedin(true)
+          navigate("/")
+        }
+      }else{
+
+      }
+    } catch (error) {
+      alert(data.message);
+    }
+  }
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-green-300 via-blue-500 to-purple-400 flex items-center justify-center">
-      <div className="bg-gray-800 p-6 rounded-lg shadow-md text-white min-w-90">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-md text-white min-w-90 md:min-w-96">
         {login === "login" ? (
           <h1 className="text-2xl font-bold text-white text-center">Login</h1>
         ) : (
@@ -15,18 +45,19 @@ const Login = () => {
             Create Account
           </h1>
         )}
-        <form action="" method="get">
+        <form onSubmit={onSubmitHander}>
           {login === "signup" && (
             <input
+            onChange={(e) => setName(e.target.value)}
               className="border-2 border-white mt-4 w-full rounded-md px-2 py-2"
               placeholder="Full Name"
               type="text"
               name="fullName"
               id="fullName"
             />
-            
           )}
           <input
+          onChange={(e) => setEmail(e.target.value)}
             className="border-2 border-white mt-4 w-full rounded-md px-2 py-2"
             placeholder="Email"
             type="email"
@@ -35,22 +66,21 @@ const Login = () => {
           />
           <br />
           <input
+          onChange={(e) => setPassword(e.target.value)}
             className="border-2 border-white mt-4 w-full rounded-md px-2 py-2"
             placeholder="Password"
             type="password"
             name="password"
             id="passworrd"
           />
-          {
-            login === "login" && (
-              <input
-              className="text-blue-300 my-4"
+          {login === "login" && (
+            <input
+              className="text-blue-300 my-4  cursor-pointer"
               onClick={() => navigate("/reset-password")}
               type="button"
               value="Forgot Password"
             />
-            )
-          }
+          )}
           <br />
           {login === "login" ? (
             <input
@@ -95,3 +125,6 @@ const Login = () => {
 };
 
 export default Login;
+
+
+
